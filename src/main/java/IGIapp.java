5cda41c76a634665a2179aae027de115ec2e58b4
iginot <igor.truchlik@gmail.com>
2020-03-09 15:48:53 +0100

    Implements #1 and #2

--- src/main/java/IGIapp.java
+++ src/main/java/IGIapp.java
@@ -1,27 +1,42 @@
 import java.util.concurrent.TimeUnit;
 
 /**
- * This class is the main class of IGIapp todo list application, currently the only class.
+ * This class is the main class of IGIapp todo list application.
  */
 
 public class IGIapp {
 
+    private Keyboard scanner;
+    private String toDelete;
+
+
+    public IGIapp()
+    {
+        scanner = new Keyboard();
+
+
+    }
+
+
     /**
      * Print the welcome message on the screen.
      */
-    private static void welcomeText() {
+    private void welcomeText() {
         System.out.println("*****************");
         System.out.println("Welcome to IGIapp");
         System.out.println("You have 0 tasks todo and 0 tasks are done!");
         System.out.println("No options to pick at the moment");
         System.out.println("*****************");
+
+        toDelete = scanner.getInput();
+        System.out.println("\n You have written: " + toDelete);
     }
 
     /**
      * Keep the app active for another 10 seconds to see the result of the action.
      * It will be decommissioned when the text input will be functional.
      */
-    private static void wait10Seconds() {
+    private void wait10Seconds() {
         System.out.println("App closes in 10 seconds");
         try{
             TimeUnit.SECONDS.sleep(10);
@@ -31,8 +46,11 @@ public class IGIapp {
     }
 
     public static void main(String[] args) {
-        welcomeText();
-        wait10Seconds();
+
+        IGIapp toDoList = new IGIapp();
+        toDoList.welcomeText();
+
+
 
     }
 }
