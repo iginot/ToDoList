872b719e2a4ace7302f94f73d60cbaeb93353b09
iginot <igor.truchlik@gmail.com>
2020-03-22 11:11:34 +0100

    Implement #10
    Mark task as done was implemented to the application.
    Issue that app was not showing right number of tasks at startup was fixed.
    Small changes in texts and menu options for the users.

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 /**
  * List all available user commands in application.
  */
+
 public class Command {
 
     Database appDatabase;
@@ -13,7 +14,8 @@ public class Command {
         appDatabase = new Database();
         ioOperator = new IOoperations();
         commandKeyboard = new Keyboard();
-        appDatabase = ioOperator.databaseRead();
+
+        appDatabase = ioOperator.databaseReadFromFile();
     }
 
     public boolean processCommand(String inputFromUser) {
@@ -31,12 +33,14 @@ public class Command {
                 System.out.println("Task successfully added.");
                 return true;
             case "3":
-                System.out.println("What task do you want to remove?");
-                String taskNumber = commandKeyboard.getInput();
-                appDatabase.removeTask(taskNumber);
+                System.out.println("What to you want to do?\n" +
+                        "1) mark as done\n" +
+                        "2) delete\n" +
+                        "0) return to previous menu");
+                processEditCommand(commandKeyboard.getInput());
                 return true;
             case "4":
-                ioOperator.databaseSave(appDatabase);
+                ioOperator.databaseSaveToFile(appDatabase);
                 System.out.println("You have chosen to quit. Have a nice day.");
                 return false;
             default:
@@ -45,17 +49,45 @@ public class Command {
         }
     }
 
+    public void processEditCommand(String inputFromUser) {
+        switch (inputFromUser) {
+            case "1":
+                System.out.println("Which task do you want to mark as done?");
+                String taskNumber = commandKeyboard.getInput();
+                appDatabase.markTaskAsDone(taskNumber);
+                break;
+            case "2":
+                System.out.println("Which task do you want to delete?");
+                taskNumber = commandKeyboard.getInput();
+                appDatabase.removeTask(taskNumber);
+                break;
+            case "0":
+                break;
+            default:
+                System.out.println("I don't understand. You can choose between options 1, 2 and 4.");
+                processEditCommand(commandKeyboard.getInput());
+        }
+    }
+
     private LocalDate getDeadline()
     {
-        LocalDate processedDate = null;
+        LocalDate processedDate;
         System.out.println("When is the deadline? Date has format 2020-02-29");
         String deadlineFromUser = commandKeyboard.getInput();
-        LocalDate convertedDate;
         try {
             processedDate = LocalDate.parse(deadlineFromUser);
         } catch (Exception e) {
             processedDate = LocalDate.now();
         }
         return processedDate;
+    }
+
+    public void showWelcomeText() {
+        int countOfOpenedTasks = appDatabase.getTasksCount(false);
+        int countOfClosedTasks = appDatabase.getTasksCount(true);
+        System.out.println("*****************");
+        System.out.println("Welcome to IGIapp");
+        System.out.println("You have " + countOfOpenedTasks + " tasks to do and " + countOfClosedTasks + " tasks are done!");
+        System.out.println("*****************");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,27 +9,32 @@ public class Task implements Serializable
 {
     private String title;
     private String project;
-    private boolean status;
+    private boolean isCompleted;
     private LocalDate date;
 
     public Task(String title, String project, LocalDate deadline)
     {
         this.title = title;
         this.project = project;
-        status = false;
+        isCompleted = false;
         this.date = deadline;
     }
 
     public String toString() {
-        if (status) {
+        if (isCompleted) {
             return ("You have done " + title + " with deadline " + date + " regarding project " + project + ".");
         } else {
             return ("You need to do " + title + " until " + date + " regarding project " + project + ".");
 
             }
         }
+    public boolean getStatus()
+    {
+        return isCompleted;
+    }
 
-    public String taskSummary(){
-        return (title + ";" + project + ";");
+    public void markAsDone()
+    {
+        isCompleted = true;
     }
 }
